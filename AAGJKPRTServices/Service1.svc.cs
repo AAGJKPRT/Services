56a511ddfecb8abbc8e10ec495a4478ed33f077b
Sajwan <sajwan@outlook.com>
2015-07-23 08:15:23 +0530

    ValidateUSer_Code

--- AAGJKPRTServices/Service1.svc.cs
+++ AAGJKPRTServices/Service1.svc.cs
@@ -102,5 +102,50 @@ namespace AAGJKPRTServices
             }
             return userInfoDataContract;
         }
+
+        public UserInfoDataContract Validate_User(string username,string pwd)
+        {
+            UserInfoDataContract userInfoDataContract = new UserInfoDataContract();
+            userinfo userinfo;
+            try
+            {
+                if(username=="jai" && pwd=="123")
+                {
+                    userInfoDataContract.Message = "Hello Jai !";
+                    userInfoDataContract.Status = true;
+                    userinfo = new userinfo();
+                    userinfo.UserId = 2;
+                    userinfo.fname = "Jaipal";
+                    userinfo.mname = "";
+                    userinfo.lname = "Sajwan";
+                    userinfo.age = 24;
+                    userinfo.DOB = "01/09/1990";
+                    userInfoDataContract.Data.Add(userinfo);
+                }
+                else if(username!="jai" && pwd!="123")
+                {
+                    userInfoDataContract.Message = "User not found !";
+                    userInfoDataContract.Status = true;
+                }
+                else if(username!="jai")
+                {
+                    userInfoDataContract.Message = "Username is not correct !";
+                    userInfoDataContract.Status = true;
+                }
+                else if(pwd!="123")
+                {
+                    userInfoDataContract.Message = "Password is not correct !";
+                    userInfoDataContract.Status = true;
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger Err = new Logger();
+                Err.ErrorLog(HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["ErrorLogPath"]), exception.Message, exception.StackTrace);
+                userInfoDataContract.Message = "Error occured and logged please connect the Service Manager !";
+                userInfoDataContract.Status = false;
+            }
+            return userInfoDataContract;
+        }
     }
 }
